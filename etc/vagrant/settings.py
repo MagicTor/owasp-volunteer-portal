dcd454bb80a9dc38433cd2a49fb4380f3e94d212
Krystian Rozycki <krystian.rozycki@stxnext.pl>
2016-12-08 18:42:12 +0100

    update on settings

--- etc/vagrant/settings.py
+++ etc/vagrant/settings.py
@@ -1,25 +1,8 @@
 """
-Development Settings Module
+Vagrant-based Development Settings Module
 """
+from volontulo_org.settings.dev import *
 
-from .base import *
-
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
-DEBUG_TOOLBAR_PATCH_SETTINGS = False
-
-INSTALLED_APPS += (
-    'debug_toolbar',
-    'django_coverage',
-    'django_extensions',
-    'django_nose'
-)
-
-TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
-
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 DATABASES = {
     'default': {
@@ -31,6 +14,3 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-
-EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
-EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'fake_emails')
