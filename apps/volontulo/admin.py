1a9dc0d02773a49f5325ff26833adf02239d9e70
Sean Auriti <sean@alphaonelabs.com>
2017-01-29 16:22:03 -0500

    show more info in the admin

--- apps/volontulo/admin.py
+++ apps/volontulo/admin.py
@@ -6,15 +6,12 @@
 
 from django.contrib import admin
 
-from apps.volontulo.models import (
-    UserProfile,
-    Organization,
-    Offer,
-    OfferImage
-)
-
+from apps.volontulo.models import *
 
 admin.site.register(UserProfile)
 admin.site.register(Organization)
 admin.site.register(Offer)
 admin.site.register(OfferImage)
+admin.site.register(UserGallery)
+admin.site.register(OrganizationGallery)
+admin.site.register(Page)
